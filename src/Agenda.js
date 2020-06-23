64b073276e639ed61f8178aa630e54563e6b8a0a
Juan Pablo <juanpabloalvira@gmail.com>
2020-06-23 19:52:23 -0300

    Ver5. Fully Functional

--- src/Agenda.js
+++ src/Agenda.js
@@ -1,16 +1,16 @@
 
 var contador = 0;
-var contacto1;
-let ContactosLista = [``];
+var Cont2 = 0;
+let ContactosLista = [];
 
 function templ(contador) {
-  return `<div class="card" style="width: 18rem;">
+  return `<div id="CaRta${contador}" class="card" style="width: 18rem;">
 <img class="card-img-top" src="${contador}.png" alt="Card image cap">
 <div class="card-body">
     <h5 id="Nombre${contador}" class="card-title">Nombre del contacto</h5>
     <p id="Tel${contador}" class="card-text">Telefono</p>
     <p id="Gmail${contador}" class="card-text">Gmail</p>
-    <a id="Editar${contador}" href="#" class="btn btn-primary" onclick="abrireditar()">Editar</a>
+    <a id="Editar${contador}" href="#" class="btn btn-primary" onclick="abrireditar(${contador})">Editar</a>
     <div id="Edits${contador}">
         <form class="form-inline my-2 my-lg-0">
             <p class="card-text">Nombre:</p>
@@ -19,16 +19,17 @@ function templ(contador) {
             <input class="form-control mr-sm-2" id="Tele${contador}">
             <p class="card-text">Gmail:</p>
             <input class="form-control mr-sm-2" id="GMail${contador}">
-            <a id="Editar${contador}" href="#" class="btn btn-primary" onclick="actualizar(); cerrarguardar()">Guardar</a>
+            <a id="Editar${contador}" href="#" class="btn btn-primary" onclick="actualizar(${contador}); cerrarguardar(${contador})">Guardar</a>
         </form>
     </div>
 </div>
+<a href="#" class="btn btn-primary" onclick="borrar(${contador})">Borrar</a>
 </div>`;
 }
 
 class contacto {
   constuctor() {
-    this.iD = ContactosLista.length;
+    this.iD = contador;
     this.nombre = "";
     this.telefono = "";
     this.gmail = "";
@@ -39,34 +40,35 @@ function CrearNuevo() {
   contador = contador + 1;
   ContactoNuevo = new contacto();
   ContactosLista.push(ContactoNuevo);
+  console.log(ContactoNuevo);
   document.getElementById("Cartas").innerHTML += templ(contador);
   document.getElementById(`Edits${contador}`).style.display = "none";
 }
 
-function abrireditar() {
-  document.getElementById(`Edits${contador}`).style.display = "Block";
-  document.getElementById(`Editar${contador}`).style.display = "none";
+function abrireditar(Y) {
+  document.getElementById(`Edits${Y}`).style.display = "Block";
+  document.getElementById(`Editar${Y}`).style.display = "none";
 }
 
-function cerrarguardar() {
-  document.getElementById(`Edits${contador}`).style.display = "none";
-  document.getElementById(`Editar${contador}`).style.display = "Block";
+function cerrarguardar(Z) {
+  document.getElementById(`Edits${Z}`).style.display = "none";
+  document.getElementById(`Editar${Z}`).style.display = "Block";
 }
 
-function actualizar() {
-  ContactosLista[contador].nombre = document.getElementById(
-    `Nom${contador}`
-  ).value;
-  ContactosLista[contador].telefono = document.getElementById(
-    `Tele${contador}`
-  ).value;
-  ContactosLista[contador].gmail = document.getElementById(
-    `GMail${contador}`
-  ).value;
+function actualizar(X) {
+  ContactosLista[X - 1].nombre = document.getElementById(`Nom${X}`).value;
+  ContactosLista[X - 1].telefono = document.getElementById(`Tele${X}`).value;
+  ContactosLista[X - 1].gmail = document.getElementById(`GMail${X}`).value;
 
-  document.getElementById(`Nom${contador}`).innerHTML = ContactosLista[contador].nombre;
-  document.getElementById(`Tele${contador}`).innerHTML = ContactosLista[contador].telefono;
-  document.getElementById(`GMail${contador}`).innerHTML = ContactosLista[contador].gmail;
+  document.getElementById(`Nombre${X}`).innerHTML = ContactosLista[X - 1].nombre;
+  document.getElementById(`Tel${X}`).innerHTML = ContactosLista[X - 1].telefono;
+  document.getElementById(`Gmail${X}`).innerHTML = ContactosLista[X - 1].gmail;
+  console.log(ContactosLista);
+  console.log(ContactosLista[X - 1].nombre);
 }
 
-function editar() {}
+function borrar(V) {
+  var element = document.getElementById(`CaRta${V}`);
+  ContactosLista.splice(V, 1);
+  element.parentNode.removeChild(element);
+}
