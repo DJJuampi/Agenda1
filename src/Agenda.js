2017a17ff92eb47528708f3c499cd5bfcd42edf4
Juan Pablo <juanpabloalvira@gmail.com>
2020-06-23 17:42:12 -0300

    Ver2

--- src/Agenda.js
+++ src/Agenda.js
@@ -1,34 +1,70 @@
 document.getElementById("Edits").style.display = "none";
+var contador = 0;
 var contacto1;
+let ContactosLista = [``];
 
+ContactoNuevo = new contacto(0, );
+    ContactosLista.push(ContactoNuevo);
+
+    function templ(contador){
+
+ return `<div class="card" style="width: 18rem;">
+<img class="card-img-top" src="${contador}.png" alt="Card image cap">
+<div class="card-body">
+    <h5 id="Nombre${contador}" class="card-title">Nombre del contacto</h5>
+    <p id="Tel${contador}" class="card-text">Telefono</p>
+    <p id="Gmail${contador}" class="card-text">Gmail</p>
+    <a id="Editar${contador}" href="#" class="btn btn-primary" onclick="abrireditar()">Editar</a>
+    <div id="Edits${contador}">
+        <form class="form-inline my-2 my-lg-0">
+            <p id="Nom${contador}" class="card-text">Nombre:</p>
+            <input class="form-control mr-sm-2">
+            <p id="Tele${contador}" class="card-text">Telefono:</p>
+            <input class="form-control mr-sm-2">
+            <p id="GMail${contador}" class="card-text">Gmail:</p>
+            <input class="form-control mr-sm-2">
+            <a id="Editar${contador}" href="#" class="btn btn-primary" onclick="actualizar(); cerrarguardar()">Guardar</a>
+        </form>
+    </div>
+</div>
+</div>`;
+}
 class contacto{
-    constuctor(nombre, telefono, gmail){
-        this.nombre = nombre;
-        this.telefono= telefono;
-        this.gmail= gmail;
+    constuctor(){
+        this.iD = ContactosLista.length;
+        this.nombre="";
+        this.telefono="";
+        this.gmail="";
     }
 }
 
+
 function CrearNuevo(){
-    //aca tiene que ir lo que hace una carta nueva, pero no se como
-    contacto1 = new contacto(document.getElementById("Nombre").innerHTML, document.getElementById("Tel").innerHTML, document.getElementById("Gmail").innerHTML)
-}
+    contador = contador + 1;
+    ContactoNuevo = new contacto();
+    ContactosLista.push(ContactoNuevo);
+    document.getElementById("Cartas").innerHTML += templ(contador);
+};
 
 function abrireditar(){
     document.getElementById("Edits").style.display = "Block";
     document.getElementById("Editar").style.display = "none";
-}
+};
 
 function cerrarguardar(){
     document.getElementById("Edits").style.display = "none";
     document.getElementById("Editar").style.display = "Block"
-}
+};
 
 
 function actualizar(){
-    contacto1.nombre = document.getElementById("Nom").innerHTML;
-    contacto1.telefono = document.getElementById("Tele").innerHTML;
-    contacto1.gmail = document.getElementById("GMail").innerHTML;
+    ContactosLista[contador].nombre = document.getElementById(`Nom${contador}`).innerHTML;
+    ContactosLista[contador].telefono = document.getElementById(`Tele${contador}`).innerHTML;
+    ContactosLista[contador].gmail = document.getElementById(`GMail${contador}`).innerHTML;
+
+    document.getElementById("Nombre").innerHTML = ContactosLista[contador].nombre;
+    document.getElementById("Tel").innerHTML = ContactosLista[contador].telefono;
+    document.getElementById("Gmail").innerHTML = ContactosLista[contador].gmail;
 }
 
 function editar(){
